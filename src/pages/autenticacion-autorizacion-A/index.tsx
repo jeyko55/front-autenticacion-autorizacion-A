ea274cb7522bf36403fc9d5f8b7eea96585f3e41
Jonatan Leal <jonatan.leal@udea.edu.co>
2024-09-15 18:47:34 -0500

    add workflows

---
+++ src/pages/autenticacion-autorizacion-A/index.tsx
@@ -0,0 +1,7 @@
+export default function autenticacionAutorizacionA() {
+    return (
+      <div className='flex justify-center items-center'>
+        Autenticación y autorización
+      </div>
+    );
+  }
---
+++ src/pages/autenticacion-autorizacion-A/signin/index.tsx
@@ -0,0 +1,7 @@
+export default function Index() {
+    return (
+      <div className='flex justify-center items-center'>
+        formulario de signin
+      </div>
+    );
+  }
