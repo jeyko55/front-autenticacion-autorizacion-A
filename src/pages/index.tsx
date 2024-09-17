810169fa4f6d0284eb875bd4a467d709616263c7
Jacobo Palacio <jacobo.palacio@udea.edu.co>
2024-09-17 07:33:00 -0500

    Prueba despliegue

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,6 @@ export default function Home() {
   return (
     <div className='flex justify-center items-center'>
       <h1 className='text-6xl'>Hola Mundo</h1>
-      <Button>Button</Button>
     </div>
   );
 }
